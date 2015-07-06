b5e2583049c83f8b4045833dab695a6423bb4730
JackWong <me@iluckly.com>
2015-07-06 20:46:45 +0800

    first version

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,22 +1,24 @@
-var exec = require("child_process").exec;
-var queryString = require("querystring");
-function start(response,postData) {
+var queryString = require("querystring"),
+  fs = require("fs"),
+  formidable = require("formidable"),
+  exec = require("child_process").exec;
+function start(response) {
   console.log("Request handler 'start' was called.");
-
   exec("",function(error,stdout,stderr) {
-    console.log("exec /");
+    // console.log("exec /");
     var body = '<html>'+
     '<head>'+
     '<meta http-equiv="Content-Type" content="text/html" charset="UTF-8"/>'+
     '</head>'+
     '<body>'+
-    '<form action="/upload" method="post">'+
-    '<textarea name="text" rows="20" cols="60"> </textarea>'+
+    '<form action="/upload" enctype="multipart/form-data" method="post">'+
+    // '<textarea name="text" rows="20" cols="60"> </textarea>'+
     '</br>'+
     '</br>'+
     '</br>'+
     '</br>'+
-    '<input type="submit" value="Submit text"/>'+
+    '<input type="file" name="upload"/>'+
+    '<input type="submit" value="upload file"/>'+
     '</form>'+
     '</body>'+
     '</html>';
@@ -44,12 +46,43 @@ function start(response,postData) {
   // sleep(10000);
   // return "Hello start!";
 }
-function upload(response,postData) {
+function upload(response,request) {
   console.log("Request handler 'upload' was called.");
-  response.writeHead(200,{"Content-Type":"text/plain"});
-    response.write("You've sent: "+queryString.parse(postData).text);
+
+  var form = new formidable.IncomingForm();
+  console.log("about to parse");
+  form.parse(request,function(error,fields,files){
+    fs.rename(files.upload.path,"/tmp/test.png",function(err){
+      if (err) {
+        fs.unlink("/tmp/test.png");
+        fs.rename(files.upload.path, "/tmp/test.png");
+      }
+    });
+    response.writeHead(200,{"Content-Type":"text/html"});
+    response.write("received image:<br/>");
+    response.write("<img src='/show' />")
     response.end();
+  });
+
+
 }
 
+function show(response) {
+  console.log("Request handler 'show' was called.");
+  response.writeHead(200,{"Content-Type":"image/png"});
+  fs.createReadStream("/tmp/test.png").pipe(response);
+  // fs.readFile("/tmp/test.png","binary",function(error, file) {
+  //   if (error) {
+  //     response.writeHead(500,{"Content-Type":"text/plain"});
+  //     response.write(error+"\n");
+  //     response.end();
+  //   }else {
+  //     response.write(200,{"Content-Type":"image/png"});
+  //     response.write(file,"binary");
+  //     response.end();
+  //   }
+  // });
+}
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
--- server.js
+++ server.js
@@ -6,21 +6,21 @@ function start(route,handle){
     var postData="";
     console.log("Request received");
     var pathname = url.parse(request.url).pathname;
-    console.log("Request for" + pathname +"received.");
-
-    request.setEncoding("utf8");
-    request.addListener("data",function(postDataChunk) {
-      // called when a new chunk of data was received
-      console.log("called when a new chunk of data was received");
-      console.log(postDataChunk);
-      postData += postDataChunk;
-      console.log("Received POST data chunk"+postDataChunk+".");
-    });
-    request.addListener("end",function() {
-      // called when all chunks of data have been received
-      console.log("called when all chunks of data have been received");
-      route(pathname,handle,response,postData);
-    });
+    console.log("Request fo r" + pathname +" received.");
+    route(pathname,handle,response,request);
+    // request.setEncoding("utf8");
+    // request.addListener("data",function(postDataChunk) {
+    //   // called when a new chunk of data was received
+    //   console.log("called when a new chunk of data was received");
+    //   console.log(postDataChunk);
+    //   postData += postDataChunk;
+    //   console.log("Received POST data chunk"+postDataChunk+".");
+    // });
+    // request.addListener("end",function() {
+    //   // called when all chunks of data have been received
+    //   console.log("called when all chunks of data have been received");
+    //   route(pathname,handle,response,postData);
+    // });
 
     // response.writeHead(200,{"Content-Type":"text/plain"});
     // response.write(content);
